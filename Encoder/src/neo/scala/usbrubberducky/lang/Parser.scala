9d113cfbb0f2cad7877a1b08c8939fd1682be759
Emil Lundberg <lundberg.emil@gmail.com>
2015-06-29 23:02:37 +0200

    Implement parsing of default delay

--- Encoder/src/neo/scala/usbrubberducky/lang/Parser.scala
+++ Encoder/src/neo/scala/usbrubberducky/lang/Parser.scala
@@ -19,17 +19,71 @@ package lang
 
 import scala.io.Source
 
-import util.{Pipeline,Context}
+import util.Context
+import util.Pipeline
+import util.NoPosition
+
 import Tokens._
 import ast.Trees._
 
 object Parser extends Pipeline[Iterator[Token], Script] {
 
   override def run(ctx: Context)(tokens: Iterator[Token]) = {
-    tokens foreach { token =>
-      println(token)
+
+    var bufferedToken: Option[Token] = None
+
+    def currentToken(): Option[Token] = bufferedToken orElse readToken()
+
+    def readToken(): Option[Token] = {
+      bufferedToken =
+        if(tokens.hasNext) {
+          tokens.next match {
+            case OfKind(BAD) => readToken() // Skip bad tokens
+            case goodToken   => Some(goodToken)
+          }
+        } else {
+          None
+        }
+      bufferedToken
     }
-    Script(None, Nil)
+
+    def eat[T](expected: TokenKind*)(thenn: Token => Option[T]): Option[T] =
+      currentToken() flatMap { token =>
+        if(expected contains token.kind) {
+          bufferedToken = None
+          thenn(token)
+        } else {
+          ctx.reporter.error(s"Expected ${expected mkString " or "}, got ${token.kind}")
+          None
+        }
+      } orElse {
+        ctx.reporter.error(s"Expected ${expected mkString " or "}, but reached end of input.")
+        None
+      }
+
+    def readIntLit[T](andThen: (IntLit => T)): Option[T] = eat(INTLITKIND) { token =>
+        token match {
+          case intLit: IntLit => Some(andThen(intLit))
+          case _ => {
+            ctx.reporter.error(s"Expected integer literal, got ${token.kind}", token.pos)
+            None
+          }
+        }
+      }
+
+    def parseDefaultDelay(): Option[DefaultDelay] =
+      currentToken() match {
+          case Some(defaultDelay@OfKind(DEFAULTDELAY)) => {
+            readToken()
+            readIntLit { intLit => DefaultDelay(intLit) }
+          }
+          case _                                 => None
+        }
+
+    Script(
+      defaultDelay = parseDefaultDelay(),
+      statements   = Nil
+    )
   }
 
 }
