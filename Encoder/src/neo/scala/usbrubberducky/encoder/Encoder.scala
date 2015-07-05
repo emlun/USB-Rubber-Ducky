45a1115216104ead3dc68465072d4f3150125e95
Emil Lundberg <lundberg.emil@gmail.com>
2015-07-05 15:15:30 +0200

    Implement encoding of DefaultDelay and Delay

--- Encoder/src/neo/scala/usbrubberducky/encoder/Encoder.scala
+++ Encoder/src/neo/scala/usbrubberducky/encoder/Encoder.scala
@@ -17,14 +17,35 @@
 package usbrubberducky
 package encoder
 
-import util.{Pipeline,Context}
 import ast.Trees._
+import lang.Tokens._
+import util.Context
+import util.Pipeline
+
 
 object NewEncoder extends Pipeline[Script, List[Byte]] {
 
-  override def run(ctx: Context)(script: Script) = {
-    println(script)
+  private def encodeDelay(milliseconds: Int): List[Byte] =
+    List.fill(milliseconds / 255)(List(0x00.toByte, 0xFF.toByte)).flatten ++:
+    (milliseconds % 255 match {
+      case 0         => Nil
+      case remainder => List(0x00.toByte, remainder.toByte)
+    }) ++:
     Nil
+
+  private def encodeStatement(defaultDelay: Option[DefaultDelay])(statement: Statement): List[Byte] = {
+    val defaultDelayBytes = defaultDelay map { delay => encodeDelay(delay.milliseconds.value) } getOrElse Nil
+
+    statement match {
+      case Delay(IntLit(milliseconds, _)) => encodeDelay(milliseconds)
+    }
+  }
+
+  override def run(ctx: Context)(script: Script) = {
+
+    val bytes: List[Byte] = script.statements flatMap encodeStatement(script.defaultDelay) _
+
+    bytes
   }
 
 }
